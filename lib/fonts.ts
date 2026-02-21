dd16e9647181ccb1e3307b5db6a282159a55dcdc
kiha40-777 <y.toriyama183501@gmail.com>
2026-02-21 16:22:00 +0900

    import inter

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,4 +1,4 @@
-import { Roboto, Noto_Sans_JP } from 'next/font/google';
+import { Roboto, Noto_Sans_JP, Inter } from 'next/font/google';
 
 export const roboto = Roboto({ // とりあえず
   subsets: ['latin'],
@@ -10,4 +10,10 @@ export const notoJP = Noto_Sans_JP({
   subsets: ['latin'],
   weight: ['400', '500', '700'],
   variable: '--font-notojp'
+});
+
+export const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '700', '900'],
+  variable: '--font-inter'
 });
