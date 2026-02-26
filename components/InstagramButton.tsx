85df2f251211e8f86bb56111d91b59f0aea36ee8
kiha40-777 <y.toriyama183501@gmail.com>
2026-02-26 19:35:43 +0900

    lg以上でロゴを大きく

--- components/InstagramButton.tsx
+++ components/InstagramButton.tsx
@@ -9,8 +9,7 @@ export default function InstagramButton({ textColor, hoverColor }: { textColor: 
         >
             <svg 
                 xmlns="http://www.w3.org/2000/svg" 
-                width="24" 
-                height="24" 
+                className="w-6 h-6 lg:w-8 lg:h-8" 
                 viewBox="0 0 24 24" 
                 fill="none" 
                 stroke="currentColor" 
--- components/MailButton.tsx
+++ components/MailButton.tsx
@@ -7,8 +7,7 @@ export default function MailButton({ textColor, hoverColor }: { textColor: strin
             >
             <svg 
                 xmlns="http://www.w3.org/2000/svg" 
-                width="24" 
-                height="24" 
+                className="w-6 h-6 lg:w-8 lg:h-8" 
                 viewBox="0 0 24 24" 
                 fill="none" 
                 stroke="currentColor" 
