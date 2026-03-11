c25cb1bcc219e1069bdaee14907b1fad3f47c4d0
kiha40-777 <y.toriyama183501@gmail.com>
2026-03-11 19:28:33 +0900

    今は英語対応しないようにする

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -28,7 +28,7 @@ export default function Header() {
             <div className="flex items-center space-x-4 lg:space-x-5">
               <NavBar /> {/* PCでのナビゲーション */}
               <ToggleMenu /> {/* タブレット・スマホでのトグルメニュー */}
-              <LanguageSwitcher />
+              {/* <LanguageSwitcher /> */}
             </div>
 
           </div>
